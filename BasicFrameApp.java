a30a8a7d655af9f3f100451cdd47fab3685dbea7
Fortu365 <fortunatemahlodi46@gmail.com>
2024-07-31 06:05:12 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,19 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        // TODO code application logic here 
+       
+        // Declare variables 
+        MyFirstFrame myFrame; 
+        // Instantiate variables
+        myFrame = new MyFirstFrame();
+        
+        
+    }
+    
+}
